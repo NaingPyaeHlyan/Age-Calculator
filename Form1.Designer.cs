94a9b8b0cedfeb157db789f8b7af05c5b10c7500
NaingPyaeHlyan <36881947+NaingPyaeHlyan@users.noreply.github.com>
2019-04-19 02:31:23 +0630

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace AgeCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnCal = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDoB = new System.Windows.Forms.TextBox();
+            this.txtAiY = new System.Windows.Forms.TextBox();
+            this.txtAiM = new System.Windows.Forms.TextBox();
+            this.txtAiD = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Yellow;
+            this.panel1.Controls.Add(this.btnExit);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.btnCal);
+            this.panel1.Controls.Add(this.txtDoB);
+            this.panel1.Location = new System.Drawing.Point(4, 8);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(719, 83);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Green;
+            this.panel2.Controls.Add(this.txtAiY);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Location = new System.Drawing.Point(4, 97);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(719, 83);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Blue;
+            this.panel3.Controls.Add(this.txtAiM);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Location = new System.Drawing.Point(4, 186);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(719, 83);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Red;
+            this.panel4.Controls.Add(this.txtAiD);
+            this.panel4.Controls.Add(this.label1);
+            this.panel4.Location = new System.Drawing.Point(4, 275);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(719, 83);
+            this.panel4.TabIndex = 3;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Black;
+            this.btnExit.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnExit.Location = new System.Drawing.Point(500, 44);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.toolTip1.SetToolTip(this.btnExit, "Exit");
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnCal
+            // 
+            this.btnCal.BackColor = System.Drawing.Color.Black;
+            this.btnCal.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnCal.Location = new System.Drawing.Point(371, 44);
+            this.btnCal.Name = "btnCal";
+            this.btnCal.Size = new System.Drawing.Size(75, 23);
+            this.btnCal.TabIndex = 2;
+            this.btnCal.Text = "Calculate Age";
+            this.toolTip1.SetToolTip(this.btnCal, "Enter your Birth Date");
+            this.btnCal.UseVisualStyleBackColor = false;
+            this.btnCal.Click += new System.EventHandler(this.btnCal_Click);
+            this.btnCal.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btnCal_MouseMove);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Green;
+            this.label4.Font = new System.Drawing.Font("Franklin Gothic Medium", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Yellow;
+            this.label4.Location = new System.Drawing.Point(131, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 24);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Age In Year";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Yellow;
+            this.label3.Font = new System.Drawing.Font("Franklin Gothic Medium", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(131, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 24);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Date of Birth";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Blue;
+            this.label2.Font = new System.Drawing.Font("Franklin Gothic Medium", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(133, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 24);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Age In Month";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.Font = new System.Drawing.Font("Franklin Gothic Medium", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(133, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 24);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Age In Days";
+            // 
+            // txtDoB
+            // 
+            this.txtDoB.BackColor = System.Drawing.Color.Black;
+            this.txtDoB.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.txtDoB.Location = new System.Drawing.Point(371, 15);
+            this.txtDoB.Name = "txtDoB";
+            this.txtDoB.Size = new System.Drawing.Size(204, 20);
+            this.txtDoB.TabIndex = 1;
+            this.txtDoB.Text = "MM/dd/yyyy";
+            // 
+            // txtAiY
+            // 
+            this.txtAiY.BackColor = System.Drawing.Color.Black;
+            this.txtAiY.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.txtAiY.Location = new System.Drawing.Point(371, 33);
+            this.txtAiY.Name = "txtAiY";
+            this.txtAiY.Size = new System.Drawing.Size(204, 20);
+            this.txtAiY.TabIndex = 0;
+            // 
+            // txtAiM
+            // 
+            this.txtAiM.BackColor = System.Drawing.Color.Black;
+            this.txtAiM.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.txtAiM.Location = new System.Drawing.Point(371, 33);
+            this.txtAiM.Name = "txtAiM";
+            this.txtAiM.Size = new System.Drawing.Size(204, 20);
+            this.txtAiM.TabIndex = 1;
+            // 
+            // txtAiD
+            // 
+            this.txtAiD.BackColor = System.Drawing.Color.Black;
+            this.txtAiD.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.txtAiD.Location = new System.Drawing.Point(371, 33);
+            this.txtAiD.Name = "txtAiD";
+            this.txtAiD.Size = new System.Drawing.Size(204, 20);
+            this.txtAiD.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(728, 365);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Age Calculator";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnCal;
+        private System.Windows.Forms.TextBox txtDoB;
+        private System.Windows.Forms.TextBox txtAiY;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAiM;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAiD;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AgeCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCal_Click(object sender, EventArgs e)
+        {
+            DateTime dateOfBirth;
+            DateTime.TryParse(txtDoB.Text, out dateOfBirth);
+            DateTime currentDate = DateTime.Now;
+            TimeSpan difference = currentDate.Subtract(dateOfBirth);
+            DateTime age = DateTime.MinValue + difference;
+            int ageInYears = age.Year - 1;
+            int ageInMonths = age.Month - 1;
+            int ageInDays = age.Day - 1;
+
+            txtAiY.Text = (ageInYears).ToString();
+            txtAiM.Text = (ageInMonths).ToString();
+            txtAiD.Text = (ageInDays).ToString();
+        }
+
+        private void btnCal_MouseMove(object sender, MouseEventArgs e)
+        {
+           // toolTip1.SetToolTip(this.btnCal, "Entter to Calculate Your Age");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult result;
+            result = MessageBox.Show("Do you really want to close", "Confirmation", MessageBoxButtons.YesNo);
+            if (result == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
